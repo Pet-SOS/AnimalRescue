16dc1d5721b557ddb35aa815e0fefc3aff6d0265
Alexey Derepa <Oleksii.Derepa@GlogalLogic.com>
2019-12-10 15:30:02 +0200

    add common api response object

---
+++ Backend/AnimalRescue/AnimalRescue.Contracts/Responses/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace AnimalRescue.Contracts.Responses
+{
+    public class ApiResponse
+    {
+        [JsonProperty("_self")]
+        public string Self { get; set; }
+    }
+}
