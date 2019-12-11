a01543d51e2c2bfdcc0ae1887d6af8ce52b56c02
Viacheslav Holovnov <viacheslav.holovnov@globallogic.com>
2019-12-11 18:04:40 +0200

    AR-11 Add automapper

--- Backend/AnimalRescue/AnimalRescue.API/Controllers/ConfigurationsController.cs
+++ Backend/AnimalRescue/AnimalRescue.API/Controllers/ConfigurationsController.cs
@@ -1,4 +1,5 @@
 ﻿using AnimalRescue.Contracts;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
@@ -7,34 +8,31 @@ namespace AnimalRescue.API.Controllers
 {
     public class ConfigurationsController : ApiControllerBase
     {
+        private readonly IMapper _mapper;
         private readonly ILogger<ConfigurationsController> _logger;
         private readonly IConfigurationService _configurationService;
 
-        public ConfigurationsController(ILogger<ConfigurationsController> logger, IConfigurationService configurationService)
+        public ConfigurationsController(ILogger<ConfigurationsController> logger, IConfigurationService configurationService, IMapper mapper)
         {
             _logger = logger;
             _configurationService = configurationService;
+            _mapper = mapper;
         }
 
         [HttpGet("cms")]
         public async Task<ActionResult<Models.CmsConfigurationModel>> CmsAsync()
         {
-            using (_logger.BeginScope("Get Cms info"))
+            using (_logger.BeginScope("Get Cms configuration"))
             {
                 var modelDto = await _configurationService.GetCmsConfiguration();
                 if (modelDto == null)
                 {
+                    _logger.LogError($"Cms configuration could not be found");
+
                     return NotFound();
                 }
 
-                var modelApi = new Models.CmsConfigurationModel()
-                {
-                    Id = modelDto.Id,
-                    Phones = modelDto.Phones,
-                    SocialLinks = modelDto.SocialLinks
-                };
-
-                return Ok(modelApi);
+                return Ok(_mapper.Map<Models.CmsConfigurationModel>(modelDto));
             }
         }
     }
---
+++ Backend/AnimalRescue/AnimalRescue.API/Core/ApiMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AnimalRescue.Models.DTO;
+using AutoMapper;
+
+namespace AnimalRescue.API.Core
+{
+    public class ApiMappingProfile : Profile
+    {
+        public ApiMappingProfile()
+        {
+            CreateMap<Models.CmsConfigurationModel, CmsConfigurationModel>();
+            CreateMap<CmsConfigurationModel, Models.CmsConfigurationModel>();
+        }
+    }
+}
