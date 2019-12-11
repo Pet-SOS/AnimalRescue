b1e989ef8bb5d3d6b239796da2d250ceb8cda5ff
Viacheslav Holovnov <viacheslav.holovnov@globallogic.com>
2019-12-11 13:33:54 +0200

    AR-11 Add controller

---
+++ Backend/AnimalRescue/AnimalRescue.API/Controllers/ConfigurationsController.cs
@@ -0,0 +1,34 @@
+﻿using AnimalRescue.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AnimalRescue.API.Controllers
+{
+    public class ConfigurationsController : ApiControllerBase
+    {
+        private readonly ILogger<ConfigurationsController> _logger;
+
+        public ConfigurationsController(ILogger<ConfigurationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("cms")]
+        public async Task<ActionResult<CmsModel>> CmsAsync()
+        {
+            using (_logger.BeginScope("Get Cms info"))
+            {
+                return Ok(new CmsModel()
+                {
+                    Phones = new List<string> { "(095)42-41-478", "(068)42-41-478" },
+                    SocialLinks = new Dictionary<string, string>() {
+                    { "Facebook", "https://www.facebook.com/" },
+                    { "Instagram", "https://www.instagram.com/" },
+                    { "Twitter", "https://twitter.com" } }
+                });
+            }
+        }
+    }
+}
---
+++ Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Interfaces/Repositories/IConfigurationRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalRescue.DataAccess.Mongodb.Interfaces.Repositories
+{
+    public interface IConfigurationRepository:IBaseRepository<>
+    {
+    }
+}
