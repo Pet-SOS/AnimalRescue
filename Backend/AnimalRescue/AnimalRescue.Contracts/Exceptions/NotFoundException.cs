b55791f06391aee339ad01d429d61e2bea1f4730
Alexey Derepa <Oleksii.Derepa@GlogalLogic.com>
2019-12-16 09:45:15 +0200

    add Generic exception bilder

--- Backend/AnimalRescue/AnimalRescue.Contracts/Exceptions/NotFoundException.cs
+++ Backend/AnimalRescue/AnimalRescue.Contracts/Exceptions/NotFoundException.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using AnimalRescue.Infrastructure.Http;
+
+using System;
+using System.Net;
 
 namespace AnimalRescue.Contracts.Exceptions
 {
-    public class NotFoundException : Exception
+    public class NotFoundException : Exception, IAppException
     {
         public NotFoundException(string message)
             : base(message)
         {
         }
+
+        public string ErrorCode => ErrorCodes.NotFound;  
+        public HttpStatusCode HttpStatusCode => HttpStatusCode.NotFound;
     }
 }
