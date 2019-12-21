e4ec3176640fe52359e4f888454f76a41b2a3159
Alexey Derepa <Oleksii.Derepa@GlogalLogic.com>
2019-12-21 21:07:15 +0200

    add  a custom database attribute

--- Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Models/Blog.cs
+++ Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Models/Blog.cs
@@ -1,17 +1,25 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using AnimalRescue.DataAccess.Mongodb.Attributes;
+
+using MongoDB.Bson.Serialization.Attributes;
+
 using System.Collections.Generic;
+
+using common = AnimalRescue.Contracts.Common.Constants.PropertyConstants.Common;
 
 namespace AnimalRescue.DataAccess.Mongodb.Models
 {
-	[BsonDiscriminator("blogs")]
+    [BsonDiscriminator("blogs")]
 	public class Blog : BaseItem
 	{
+        [CouplingPropertyName(common.Body)]
 		[BsonElement("body")]
 		public string Body { get; set; }
 
+        [CouplingPropertyName(common.Description)]
 		[BsonElement("description")]
 		public string Description { get; set; }
 
+        [CouplingPropertyName(common.ImageIds)]
 		[BsonElement("imagesIds")]
 		public IList<string> ImagesIds { get; set; }
 	}
