2d9d8cece0819a0731804d14db6dfe469d1e91db
Alexey Derepa <Oleksii.Derepa@GlogalLogic.com>
2019-12-22 17:10:25 +0200

    add new filter rules: Gte, Gt ,Lte, Lt

--- Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/QueryBuilders/FilterContractConstants.cs
+++ Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/QueryBuilders/FilterContractConstants.cs
@@ -7,11 +7,19 @@ namespace AnimalRescue.DataAccess.Mongodb.QueryBuilders
     {
         public const string All = "all";
         public const string Eq = "eq";
+        public const string Gte = "gte";
+        public const string Gt = "gt";
+        public const string Lte = "lte";
+        public const string Lt = "lt";
         public static Dictionary<string, Func<Term, string>> Rulses
             = new Dictionary<string, Func<Term, string>>()
             {
                 { All, (term) => $"{term.FieldName}:{{${All}:[{string.Join(",",term.Content.Replace("(","").Replace(")","").Split("^"))}]}}"},
-                { Eq, (term) => string.Format("{0}:{1}",term.FieldName, term.Content)}
+                { Eq, (term) => string.Format("{0}:{1}",term.FieldName, term.Content)},
+                { Gte, (term) => $"{term.FieldName}:{{${Gte}:{term.Content}}}" },
+                { Gt, (term) => $"{term.FieldName}:{{${Gt}:{term.Content}}}" },
+                { Lte, (term) => $"{term.FieldName}:{{${Lte}:{term.Content}}}" },
+                { Lt, (term) => $"{term.FieldName}:{{${Lt}:{term.Content}}}" },
             };
     }
 }
