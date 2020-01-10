3abc4b3c7edb369f09cf3d0da925976232103a23
Danyil Deli <danyil.deli@globallogic.com>
2020-01-10 11:57:52 +0200

    added counter block with static data

---
+++ Frontend/src/components/CounterBlock/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.scss';
+
+interface IPropTypes {
+  backgroundColor: string;
+  count: number | string;
+  images: string[];
+  title: string | React.ReactNode;
+  text?: string | React.ReactNode;
+}
+
+export const CounterBlock: React.FC<IPropTypes> = ({ backgroundColor, count, title, images, text}) => {
+  return (
+    <div className="counter-holder" style={{backgroundColor}}>
+      <div className="counter">
+        {`${!!count ? count : 0}`.split('').map((number, index) => (
+          <div className="count-inner" key={index}>
+            <span>{number}</span>
+          </div>
+        ))}
+      </div>
+      <div className="title">{title}</div>
+      {!!text ? <span className="text"> {text}</span> : null}
+      <div className="image-holder">
+        {images.slice(0, 8).map((img, index) => (
+          <img src={img} key={index} alt='pet'/>
+        ))}
+      </div>
+    </div>
+  )
+}
