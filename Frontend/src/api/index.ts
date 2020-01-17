1457ee401d4a1554134c6a27f43272e6a1dddd64
Andrii Panaseiko <Andrii.Panaseiko@globallogic.com>
2020-01-17 11:43:45 +0200

    added .env file

--- Frontend/src/api/index.ts
+++ Frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = `https://animalrescueapi.azurewebsites.net/api/`
+export const BASE_URL = process.env.REACT_APP_API_URL
 
 export default axios.create({
     baseURL: BASE_URL
