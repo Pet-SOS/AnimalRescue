569a2a86832bf39cf78f0b4bccba359b36620d76
Alexey Derepa <Oleksii.Derepa@GlobalLogic.com>
2020-01-24 10:39:04 +0200

    add TegDecorator

---
+++ Backend/AnimalRescue/AnimalRescue.BusinessLogic/Queries/TegDecorator.cs
@@ -0,0 +1,13 @@
+﻿using AnimalRescue.Contracts.BusinessLogic.Interfaces.CRUD;
+using System.Threading.Tasks;
+
+namespace AnimalRescue.BusinessLogic.Queries
+{
+    internal class TegDecorator<TOut, TIn> : IBlCreateAsync<TOut, TIn>
+    {
+        public async Task<TOut> CreateAsync(TIn value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
