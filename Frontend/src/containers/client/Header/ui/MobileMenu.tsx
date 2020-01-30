a08b7ebf8aa8d4494640266e0927beec77cd10cc
zherng <zherniuk@gmail.com>
2020-01-30 12:45:57 +0200

    AR-74: [FE] About Services page - the missing fix in MobileMenu.tsx

--- Frontend/src/containers/client/Header/ui/MobileMenu.tsx
+++ Frontend/src/containers/client/Header/ui/MobileMenu.tsx
@@ -50,7 +50,7 @@ export const MobileMenu: React.FC = () => {
                     <div className="drop-icon">
                        <span><TI18n keyStr="headerMenuItem1" default="О службе"/></span>
                         <ul className="dropdown" onClick={(e)=> stopFloatinUp(e)}>
-                            <li><a href="rescue-service"><TI18n keyStr="headerMenuItem1Dropdown1" default="О службе спасения"/></a></li>
+                            <li><Link to="/about"><TI18n keyStr="headerMenuItem1Dropdown1" default="Про службу порятунку" /></Link></li>
                             <li><Link to="/rules"><TI18n keyStr="headerMenuItem1Dropdown2" default="Правила работы с нами" /></Link></li>
                             <li><Link to="/financial-reports"><TI18n keyStr="headerMenuItem1Dropdown3" default="Финансовые отчеты"/></Link></li>
                         </ul>
