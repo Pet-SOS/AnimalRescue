ce76095b9f5ca0ca17282ef4522e9d9b73746424
Danyil Deli <danyil.deli@globallogic.com>
2020-03-02 15:51:16 +0200

    changed footer markup

--- Frontend/src/components/SocialLinks/index.tsx
+++ Frontend/src/components/SocialLinks/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import {ReactComponent as FacebookLogo} from '../../assets/header/facebook.svg';
-import {ReactComponent as InstagramLogo} from '../../assets/header/twitter.svg';
-import {ReactComponent as YoutubeLogo} from '../../assets/header/youtube.svg';
 import './socialLinks.scss';
 import { store } from '../../store';
 import { useSelector, shallowEqual } from 'react-redux';
 import { ISocialLinks } from '../../api/contacts';
 import { selectInfoContacts } from '../../containers/client/Home/store/selectors';
 
-
 export const SocialLinks: React.FC<any> = () => {
 const socialLinks: ISocialLinks = useSelector(() => (selectInfoContacts(store.getState()).data.socialLinks), shallowEqual);
 
   return (
-    <div className="social-links">
-        <a href={socialLinks.facebook} target="_blank" ><FacebookLogo/></a>
-        <a href={socialLinks.instagram} target="_blank" ><InstagramLogo/></a>
-        <a href={socialLinks.youtube} target="_blank" ><YoutubeLogo/></a>
-    </div>
+    <ul className="social-links">
+      <li>
+        <a className='facebook' href={socialLinks.facebook} target="_blank" ></a>
+      </li>
+      <li>
+        <a className='instagram' href={socialLinks.instagram} target="_blank" ></a>
+      </li>
+      <li>
+        <a className='youtube' href={socialLinks.youtube} target="_blank" ></a>
+      </li>
+    </ul>
   )
 };
