21bc0d6def0d9e5b402d65832dbd8e7410e93224
anton.bilyi <anton.bilyi@globallogic.com>
2020-04-23 12:49:52 +0300

    add hardcoded telegram bot code

--- Backend/BackgroundServices/TelegramMessenger/Program.cs
+++ Backend/BackgroundServices/TelegramMessenger/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
 
 namespace TelegramMessenger
 {
     class Program
     {
+        private static readonly string token = "1152281063:AAFJyTDTMn2eXZk_YMYFUYRrrv46QB7HU9s";
+
+        private static readonly TelegramBotClient bot = new TelegramBotClient(token);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            bot.OnMessage += Bot_OnMessage;
+
+           // bot.SendTextMessageAsync(-473998265, "BotStarted!");
+
+            bot.StartReceiving();
+
+            Console.ReadLine();
+
+            bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object? sender, MessageEventArgs e)
+        {
+            if (e.Message.Type == MessageType.Text)
+            {
+                bot.SendTextMessageAsync(e.Message.Chat.Id, "Hello World!");
+
+                //bot.SendTextMessageAsync(-473998265, $"{e.Message.From.Username} sended: {e.Message.Text}");
+
+            }
         }
     }
 }
