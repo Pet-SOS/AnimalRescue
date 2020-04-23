91450d788477c7f6e173e034b1961fc178599cb0
anton.bilyi <anton.bilyi@globallogic.com>
2020-04-23 11:22:40 +0300

    Add BackgroundServices solution and TelegramMessenger project

---
+++ Backend/BackgroundServices/TelegramMessenger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramMessenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
