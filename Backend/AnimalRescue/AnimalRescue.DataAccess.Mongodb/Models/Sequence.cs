cd549ea802324a789ac0358f0d7d2b9f9944acec
Dmytro Korotkyi <dmytro.korotkyi@globallogic.com>
2020-06-04 20:48:39 +0300

    Sequence generation has been fixed.

--- Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Models/Sequence.cs
+++ Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Models/Sequence.cs
@@ -1,11 +1,15 @@
-﻿using AnimalRescue.DataAccess.Mongodb.Models.BaseItems;
+﻿using AnimalRescue.DataAccess.Mongodb.Attributes;
+using AnimalRescue.DataAccess.Mongodb.Models.BaseItems;
 using MongoDB.Bson.Serialization.Attributes;
+
+using animal = AnimalRescue.Contracts.Common.Constants.PropertyConstants.Animal;
 
 namespace AnimalRescue.DataAccess.Mongodb.Models
 {
     [BsonDiscriminator("sequence")]
     public class Sequence : BaseAndTimeItem
     {
+        [CouplingPropertyName(animal.Number)]
         [BsonElement("number")]
         public int Number { get; set; }
     }
