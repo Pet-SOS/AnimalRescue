53f7e77ae1e95d4bb2590621403328d6a2f1308a
Alexey <graf_dav2@ukr.net>
2020-06-04 20:03:45 +0300

    [FIX] fix filter

--- Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/QueryBuilders/StrictFilterContractConstants.cs
+++ Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/QueryBuilders/StrictFilterContractConstants.cs
@@ -102,8 +102,9 @@ namespace AnimalRescue.DataAccess.Mongodb.QueryBuilders
 
                 if (Ne == operationName)
                 {
-                    return Builders<TE>.Filter.Regex(fieldName, new BsonRegularExpression(new Regex(content, RegexOptions.IgnoreCase)));
+                    return LookFor<TE, string>(fieldName, content, (x) => x, operationName);
                 }
+
                 //return Builders<TE>.Filter.Text(content);
                 throw new BadRequestException($"this {nameof(operationName)}: '{operationName}' is not support for this property");
             }
