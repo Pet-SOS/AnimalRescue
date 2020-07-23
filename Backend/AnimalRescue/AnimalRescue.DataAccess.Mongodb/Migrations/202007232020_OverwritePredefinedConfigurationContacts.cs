2fef0a0531025c558a75d40f6a425d790b4179c5
Oleksii Derepa <Oleksii.Derepa@GlobalLogic.com>
2020-07-23 20:51:47 +0300

    [FEATURE] add cms, helpPopup, languages migration

--- Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Migrations/202007232020_OverwritePredefinedConfigurationContacts.cs
+++ Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Migrations/202007232020_OverwritePredefinedConfigurationContacts.cs
@@ -1,26 +1,28 @@
 ﻿using AnimalRescue.DataAccess.Mongodb.Interfaces.Repositories;
 using AnimalRescue.DataAccess.Mongodb.Migrations.Engine;
-using AnimalRescue.DataAccess.Mongodb.Migrations.MigrationModels.Configurations;
 using AnimalRescue.DataAccess.Mongodb.Models.Configurations;
+using AnimalRescue.DataAccess.Mongodb.Models.Configurations.Nested;
 
 using System.Threading.Tasks;
 
 namespace AnimalRescue.DataAccess.Mongodb.Migrations
 {
-    [Migration("202006101926_AddPredefinedCmsData")]
-    internal class AddPredefinedCmsData : IAnimalRescueMigration
+    [Migration("202007232020_OverwritePredefinedConfigurationContacts")]
+    internal class OverwritePredefinedConfigurationContacts : IAnimalRescueMigration
     {
+        const string fileName = "contacts_202007232020.json";
+
         private readonly IConfigurationRepository _configurationRepository;
 
-        public AddPredefinedCmsData(IConfigurationRepository configurationRepository)
+        public OverwritePredefinedConfigurationContacts(IConfigurationRepository configurationRepository)
         {
             _configurationRepository = configurationRepository;
         }
 
         public async Task Execute()
         {
-            await _configurationRepository.SetUpDataBaseFromJsonFileAsync<IConfigurationRepository, Configuration<Contacts202006101926>>(
-                "cms.json",
+            await _configurationRepository.SetUpDataBaseFromJsonFileAsync<IConfigurationRepository, Configuration<Contacts>>(
+                fileName,
                 (repo, item) => repo.CreateAsync(item));
         }
     }
