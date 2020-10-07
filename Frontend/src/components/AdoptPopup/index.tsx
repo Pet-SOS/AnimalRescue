050d41828ff83389305db296ed4d8958a195ceed
Vlad Tymoshchyk <vtymoshchyk@gmail.com>
2020-10-07 13:36:03 +0300

    Implemented submit handler for popup form

--- Frontend/src/components/AdoptPopup/index.tsx
+++ Frontend/src/components/AdoptPopup/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, shallowEqual, Provider } from 'react-redux';
+import { message } from 'antd';
 import { selectInfoContacts } from '../../containers/client/Home/store/selectors';
 import { TI18n } from '../../i18n';
 import { store } from '../../store';
 import heartImage from '../../img/heart.png';
 import './index.scss';
 import { Button, ButtonTypes } from '../Button';
+import { postAnimalAdoptionRequest } from '../../api/animals';
 
 interface IPropTypes {
-  onClose?: () => void;
+  onClose: () => void;
+  AnimalName: string;
+  AnimalId: string | undefined;
 }
 
-export const AdoptPopup: React.FC<IPropTypes> = ({ onClose }) => {
+export const AdoptPopup: React.FC<IPropTypes> = ({ onClose, AnimalId, AnimalName }) => {
   const { phones } = useSelector(() => selectInfoContacts(store.getState()).data, shallowEqual);
+  const [AdoptiveName, setAdoptiveName] = useState('');
+  const [PhoneNumber, setPhoneNumber] = useState('');
+  const [errors, setErrors] = useState<{ phone: null | JSX.Element }>({
+    phone: null,
+  })
+  const handleSubmit = async () => {
+    if (!PhoneNumber) {
+      setErrors({ ...errors, phone: <TI18n keyStr="thisFieldIsRequired" default="Обов'язкове поле" /> })
+      return;
+    } else if (!/^[+()0-9]+$/.test(PhoneNumber)) {
+      setErrors({ ...errors, phone: <TI18n keyStr="invalidPhoneFormat" default="Неправильний формат номеру" /> })
+      return;
+    }
+    setErrors({ ...errors, phone: null });
+
+    const key = 'sendingRequest';
+    message.loading({
+      content: <Provider store={store}>
+        <TI18n keyStr="loading" default="Завантаження..." />
+      </Provider>, 
+      key,
+    })
+    try {
+      await postAnimalAdoptionRequest({ 
+        AdoptiveName, 
+        PhoneNumber, 
+        AnimalId, 
+        AnimalName,
+      });
+      message.success({
+        content: (
+          <Provider store={store}>
+            <TI18n keyStr="requestHasBeenSent" default="Ваш запит успішно надіслано" />
+          </Provider>
+        ),
+        key
+      });
+    } catch {
+      message.error({
+        content: (
+          <Provider store={store}>
+            <TI18n keyStr="smthWentWrongTryLater" default="Щось пішло не так... Спробуйте, будь ласка, пізніше" />
+          </Provider>
+        ),
+        key,
+      });
+    } finally {
+      onClose();
+    }
+  }
+  const handleNameChange = (e: { target: { value: string } }) => setAdoptiveName(e.target.value);
+  const handlePhoneChange = (e: { target: { value: string } }) => setPhoneNumber(e.target.value);
+
   return (
     <div className='adopt-popup-wrapper'>
       <div className='adopt-popup-holder'>
@@ -42,9 +99,14 @@ export const AdoptPopup: React.FC<IPropTypes> = ({ onClose }) => {
             <TI18n keyStr="adoptPopupText5" default="оставьте нам свои контакты и мы сами вам перезвоним." />
           </p>
           <form>
-            <input type="text" placeholder='Имя' />
-            <input type="text" placeholder='Телефон' />
-            <Button styleType={ButtonTypes.Blue}>
+            <input type="text" name="AdoptiveName" placeholder='Имя' value={AdoptiveName} onChange={handleNameChange}/>
+            <input className={errors.phone ? 'validation-error' : ''} type="text" name="PhoneNumber" placeholder='Телефон' value={PhoneNumber} onChange={handlePhoneChange} />
+            {errors.phone && (
+              <div className="validation-message">
+                {errors.phone}
+              </div>
+            )}
+            <Button styleType={ButtonTypes.Blue} onClick={handleSubmit}>
               <TI18n keyStr="callBack" default="Перезвоните мне" />
             </Button>
           </form>
