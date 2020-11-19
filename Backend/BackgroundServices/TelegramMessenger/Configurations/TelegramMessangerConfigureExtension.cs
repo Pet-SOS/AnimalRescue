7310c52bbbcda96fe2232dbc1631d48637924737
SYNAPSE\ivan.kushnir <KUSHIVA@hilti.com>
2020-11-19 11:25:53 +0200

    [AR-158] moved DI config to extension

---
+++ Backend/BackgroundServices/TelegramMessenger/Configurations/TelegramMessangerConfigureExtension.cs
@@ -0,0 +1,38 @@
+﻿using AnimalRescue.BusinessLogic.Configurations;
+using AnimalRescue.BusinessLogic.Services;
+using AnimalRescue.Contracts.BusinessLogic.Interfaces;
+using AnimalRescue.DataAccess.Mongodb;
+using AnimalRescue.Infrastructure.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TelegramMessenger.Services;
+using TelegramMessenger.Services.Commands;
+using TelegramMessenger.Services.Interfaces;
+
+namespace TelegramMessenger.Configurations
+{
+    internal static class TelegramMessangerConfigureExtension
+    {
+        public static void AddConfigureTelegramMessanger(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            IPublisherSettings publisherSettings = configuration
+                .GetTypedSection<PublisherSettings>(nameof(PublisherSettings));
+            ITelegramPublisherSettings telegramPublisherSettings = configuration
+                .GetTypedSection<TelegramPublisherSettings>(nameof(TelegramPublisherSettings));
+
+
+            services.AddConfigureMongoDb(ConfigurationUtil.GetConfiguration());
+            services.AddScoped((p) => publisherSettings);
+            services.AddScoped((p) => telegramPublisherSettings);
+            services.AddScoped<ISenderPublisherSettingsBase>((p) => telegramPublisherSettings);
+            services.AddScoped<IEventReceivingService, EventReceivingService>();
+            services.AddScoped<IMessenger, Services.TelegramMessenger>();
+            services.AddScoped<ICommand, MessageCommand>();
+            services.AddScoped<ICommand, RegisterCommand>();
+            services.AddScoped<ICommand, HelloCommand>();
+            services.AddSingleton<ITelegramBot, TelegramBot>();
+        }
+    }
+}
--- Backend/BackgroundServices/TelegramMessenger/Program.cs
+++ Backend/BackgroundServices/TelegramMessenger/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using AnimalRescue.BusinessLogic.Configurations;
-using AnimalRescue.BusinessLogic.Services;
 using AnimalRescue.Contracts.BusinessLogic.Interfaces;
 using AnimalRescue.Contracts.BusinessLogic.Models.EventMessages;
-using AnimalRescue.DataAccess.Mongodb;
 using AnimalRescue.Infrastructure.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TelegramMessenger.Services;
-using TelegramMessenger.Services.Commands;
+using TelegramMessenger.Configurations;
 using TelegramMessenger.Services.Interfaces;
 
 namespace TelegramMessenger
@@ -18,25 +14,9 @@ namespace TelegramMessenger
 
         static void Main(string[] args)
         {
-            IPublisherSettings publisherSettings = ConfigurationUtil
-                .GetConfiguration()
-                .GetTypedSection<PublisherSettings>(nameof(PublisherSettings));
-            ITelegramPublisherSettings telegramPublisherSettings = ConfigurationUtil
-                .GetConfiguration()
-                .GetTypedSection<TelegramPublisherSettings>(nameof(TelegramPublisherSettings));
-
             var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddConfigureMongoDb(ConfigurationUtil.GetConfiguration());
-            serviceCollection.AddScoped((p) => publisherSettings);
-            serviceCollection.AddScoped((p) => telegramPublisherSettings);
-            serviceCollection.AddScoped<ISenderPublisherSettingsBase>((p) => telegramPublisherSettings);
-            serviceCollection.AddScoped<IEventReceivingService, EventReceivingService>();
-            serviceCollection.AddScoped<IMessenger, Services.TelegramMessenger>();
-            serviceCollection.AddScoped<ICommand, MessageCommand>();
-            serviceCollection.AddScoped<ICommand, RegisterCommand>();
-            serviceCollection.AddScoped<ICommand, HelloCommand>();
-            serviceCollection.AddSingleton<ITelegramBot, TelegramBot>();
+            serviceCollection.AddConfigureTelegramMessanger(ConfigurationUtil.GetConfiguration());
 
             using var serviceProvider = serviceCollection.BuildServiceProvider();
 
@@ -48,21 +28,7 @@ namespace TelegramMessenger
 
             eventReceivingService.Run<EmergencyMessage>((message) => _messenger.SendTextMessageAsync($"{message.Title} {Environment.NewLine} {message.Message} {Environment.NewLine} {message.Address}"));
 
-            bool isContinue = true;
-
-            while (isContinue)
-            {
-               var text = Console.ReadLine();
-
-               if (text == "stop")
-               {
-                   isContinue = false;
-               }
-               else
-               {
-                   _messenger.SendTextMessageAsync(text);
-               }
-            }
+            Console.ReadLine();
         }
     }
 }
