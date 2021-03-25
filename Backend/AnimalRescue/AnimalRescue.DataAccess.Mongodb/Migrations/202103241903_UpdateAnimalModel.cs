fdf613a56ffc0a1d83f870d2452d195932ccf8f3
ivan.kushnir <ivan.kushnir@icloud.com>
2021-03-25 11:15:25 +0200

    [AR-253] implemented migration, used automapper for mapping DA versoins of models

--- Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Migrations/202103241903_UpdateAnimalModel.cs
+++ Backend/AnimalRescue/AnimalRescue.DataAccess.Mongodb/Migrations/202103241903_UpdateAnimalModel.cs
@@ -2,35 +2,76 @@
 using AnimalRescue.DataAccess.Mongodb.Migrations.Engine;
 using AnimalRescue.DataAccess.Mongodb.Migrations.MigrationModels.Animal;
 using AnimalRescue.DataAccess.Mongodb.Models;
+using AnimalRescue.DataAccess.Mongodb.Models.BaseItems;
+using AnimalRescue.DataAccess.Mongodb.Models.Tag;
+using AnimalRescue.Infrastructure.Validation;
+using AutoMapper;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace AnimalRescue.DataAccess.Mongodb.Migrations
 {
-    [Migration("202003060152_AddDefaultUserAndAdminRoles")]
+    [Migration("202103241903_UpdateAnimalModel")]
     internal class UpdateAnimalModel : IAnimalRescueMigration
     {
         private readonly IBaseRepository<Animal202103241821> _animalV1Repository;
-        private readonly IBaseRepository<Animal> _animalV2Repository;
+        private readonly IMongoCollection<Animal> _animalV2Collection;
+        private IMapper _mapper;
 
-        public UpdateAnimalModel(IBaseRepository<Animal202103241821> animalV1Repository, IBaseRepository<Animal> animalV2Repository)
+
+        public UpdateAnimalModel(IBaseRepository<Animal202103241821> animalV1Repository, IMongoDatabase database)
         {
-            this._animalV1Repository = animalV1Repository;
-            this._animalV2Repository = animalV2Repository;
+            Require.Objects.NotNull(database, nameof(database));
+            Require.Objects.NotNull(animalV1Repository, nameof(animalV1Repository));
+
+            _animalV1Repository = animalV1Repository;
+
+            var collectionName = typeof(Animal).GetCustomAttribute<BsonDiscriminatorAttribute>()?.Discriminator;
+
+            Require.Strings.NotNullOrWhiteSpace(collectionName, nameof(collectionName));
+
+            _animalV2Collection = database.GetCollection<Animal>(collectionName);
+
+            _mapper = GetMapper();
         }
 
         public async Task Execute()
         {
             await foreach (var animalV1 in _animalV1Repository.GetAllItemsAsync())
             {
-                var animal = Map(animalV1);
+                var animal = _mapper.Map<Animal>(animalV1);
 
-                await _animalV2Repository.UpdateAsync(animal);
+                await _animalV2Collection.ReplaceOneAsync(a => a.Id == animal.Id, animal);
             }
         }
 
-        private Animal Map(Animal202103241821 animalV1)
+        private IMapper GetMapper()
         {
-            return new Animal();
+            var mapperConfiguration = new MapperConfiguration(
+                mc =>
+                {
+                    mc.CreateMap<Animal202103241821, Animal>()
+                    .ForMember(
+                        x => x.Names,
+                        o => o.MapFrom(
+                            x => new List<LanguageValue>
+                            { 
+                                new LanguageValue
+                                {
+                                    Lang = "ua",
+                                    Value = x.Name
+                                } 
+                            })
+                    );
+
+                    mc.CreateMap<WellKnownTag, WellKnownTag>();
+                    mc.CreateMap<LanguageValue, LanguageValue>();
+                });
+
+            return mapperConfiguration.CreateMapper();
         }
     }
 }
