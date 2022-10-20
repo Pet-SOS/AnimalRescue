70708514a6fd2f9b95bf8fed23be865c7590b457
Dmytro Chepil <dmytro.chepil@customerxi.com>
2022-10-20 10:44:10 +0300

    fixed 0 symbol in empty page case

--- Frontend/src/containers/client/LookingForAFriend/ui/LookingForAFriendPage.tsx
+++ Frontend/src/containers/client/LookingForAFriend/ui/LookingForAFriendPage.tsx
@@ -624,7 +624,7 @@ export class LookingForAFriendPage extends React.Component<IPropTypes> {
           </div>
           <div className="hold-content-items section-margin">
             <div className="content-block-animals">
-              {this.props.animalsList.data.length &&
+              {this.props.animalsList.data.length > 0 &&
                 this.props.animalsList.data.map(animal => (
                   <div className="animal" key={animal.id}>
                     <AnimalCard animal={animal} />
